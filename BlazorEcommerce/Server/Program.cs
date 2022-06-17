ca8ee782afa893cd4e2292aacdadb2b063dfb7c9
gyanranjan478 <52964794+gyanranjan478@users.noreply.github.com>
2022-06-18 01:24:52 +0530

    Seed Data & Returning data from database. First db call from API

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,5 +1,7 @@
 global using BlazorEcommerce.Shared;
 global using Microsoft.EntityFrameworkCore;
+global using BlazorEcommerce.Server.Data;
+
 using BlazorEcommerce.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
